d8cc2b091b550e6a03b87a3e1967fcd9d2162403
dodo <biryukwolf@yahoo.com>
2017-06-07 22:30:47 +0300

    90 v del livereload

--- news.php
+++ news.php
@@ -32,7 +32,7 @@
  <link href="/css/dashboard_news.css" rel="stylesheet">
     <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
     <!--[if lt IE 9]><script src="/assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-    <script src="/bootstrap-3.3.7/docs/assets/js/ie-emulation-modes-warning.js"></script>
+    <script async src="/bootstrap-3.3.7/docs/assets/js/ie-emulation-modes-warning.js"></script>
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
