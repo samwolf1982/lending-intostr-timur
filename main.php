48832c96bbbc0e7888b0c11c73e32b5fe0df4bb0
dodo <biryukwolf@yahoo.com>
2017-06-07 04:55:30 +0300

    main ok ldel

--- main.php
+++ main.php
@@ -32,7 +32,7 @@
     <link href="/css/dashboard_main.css" rel="stylesheet">
 
 
-<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+
 
     
 
